391134ffe90a86b827b8b36925a8b23f68f2e3ae
Nick Williams <nick22891@yahoo.com>
2016-10-02 17:05:28 -0500

    Correcting typo in bower.json

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -5,6 +5,7 @@
 angular.module('nick-gdg', ["ngRoute", "nick-gdg.services"]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
     $routeProvider.when('/', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
 
     //$locationProvider.html5Mode(true);
