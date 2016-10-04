1ff7d93575247158e5543174dc27dc812b0f63e7
Nick Williams <nick22891@yahoo.com>
2016-10-03 22:50:45 -0500

    Re-committing the navigation bar toggle to correct a git error moments ago.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,16 +1,17 @@
 /**
- * Created by matjames007 on 9/10/16.
+ * Created by nick22891 on 3/10/16.
  */
 
-angular.module('nick-gdg', ["ngRoute", "nick-gdg.services", "isteven-multi-select"]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
-    $routeProvider.when('/goals/edit', {templateUrl: '../partials/editgoals.html', controller: 'GoalEditCtrl'});
-    $routeProvider.when('/login', {templateUrl: '../partials/login.html', controller: 'UserLoginCtrl'});
-    $routeProvider.when('/signup', {templateUrl: '../partials/signup.html', controller: 'UserSignupCtrl'});
-    $routeProvider.when('/logout', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
-    $routeProvider.when('/projects', {templateUrl: '../partials/projectslisting.html', controller: 'ProjectsCtrl'});
-    $routeProvider.when('/goal/:id/projects', {templateUrl: '../partials/projectslisting.html', controller: 'ProjectsCtrl'});
-    $routeProvider.otherwise({redirectTo: '/'});
+angular.module('nick-gdg', ["ngRoute", "nick-gdg.services", "isteven-multi-select","mgcrea.ngStrap"])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
+        $routeProvider.when('/goals/edit', {templateUrl: '../partials/editgoals.html', controller: 'GoalEditCtrl'});
+        $routeProvider.when('/login', {templateUrl: '../partials/login.html', controller: 'UserLoginCtrl'});
+        $routeProvider.when('/signup', {templateUrl: '../partials/signup.html', controller: 'UserSignupCtrl'});
+        $routeProvider.when('/logout', {templateUrl: '../partials/homepage.html', controller: 'HomeCtrl'});
+        $routeProvider.when('/projects', {templateUrl: '../partials/projectslisting.html', controller: 'ProjectsCtrl'});
+        $routeProvider.when('/goal/:id/projects', {templateUrl: '../partials/projectslisting.html', controller: 'ProjectsCtrl'});
+        $routeProvider.otherwise({redirectTo: '/'});
 
-    //$locationProvider.html5Mode(true);
-}]);
+        //$locationProvider.html5Mode(true);
+    }]);
